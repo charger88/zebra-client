4d48e8688a0d280d7385d4e5ea8e7f8a80ca7eac
Michael Kelner <mikhail.kelner@gmail.com>
2018-06-13 20:23:48 -0400

    Version control

--- app/app.js
+++ app/app.js
@@ -1,9 +1,25 @@
 requirejs(["router", "helpers"], function(router, helpers) {
+	const checkVersion = (version) => {
+		if (!version){
+			return false;
+		}
+		const versions = ["1.0."];
+		for (var i = 0;i < versions.length;i++){
+			if (version.indexOf(versions[i]) === 0){
+				return true
+			}
+		}
+		return false;
+	}
 	const initApp = (config) => {
+		if (!checkVersion(config.version)){
+			return false;
+		}
 		window.appConfig = config;
 		document.querySelector("title").appendChild(document.createTextNode("🦓 " + config.name));
 		document.querySelector("#instance-name a").appendChild(document.createTextNode(config.name));
 		document.querySelector("header").style.background = config.color;
+		return true;
 	}
 	const loadAPIUrl = () => {
 		var apiURL = document.location.search || "?";
@@ -21,9 +37,16 @@ requirejs(["router", "helpers"], function(router, helpers) {
 	};
 	window.apiURL = (window.location.protocol !== "https:") ? loadAPIUrl() : window.location.origin;
 	helpers.ajax("GET", window.apiURL + "/config", {}, [], (response) => {
-		initApp(response)
-		window.addEventListener("hashchange", router, false);
-		router();
+		if (initApp(response)){
+			window.addEventListener("hashchange", router, false);
+			router();
+		} else {
+			if (window.location.protocol !== "https:"){
+				alert("Version problem. Update your Zebra client's files.");
+			} else {
+				alert("Version problem. Try to clear your browser's chache.");
+			}
+		}
 	});
 	setInterval(() => {
 		document.querySelectorAll(".countdown").forEach((item) => {
